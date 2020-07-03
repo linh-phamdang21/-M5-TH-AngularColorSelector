6c100cfc76c7bc1725d3d951c5634f6a0e63197d
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-07-03 09:51:22 +0700

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'M5-TH-AngularColorSelection';
+  background = '#00e067';
+  constructor() {
+  }
+  // tslint:disable-next-line:typedef
+  onChange(value){
+    this.background = value;
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {Routes, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
